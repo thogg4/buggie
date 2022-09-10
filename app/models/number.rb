934a736d71a52ea56f6f1712f644bebc2e50a902
Tim Hogg <thogg4@gmail.com>
2022-09-10 16:21:03 -0400

    add method for cron

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,4 +1,10 @@
 class Number < ApplicationRecord
+  THUMBS = '1F44D'
+  ROCK = '1F918'
+  HANG = '1F919'
+  OK = '1F44C' 
+  RESPONSES = [THUMBS, ROCK, HANG, OK]
+
   has_many :items
 
   def send_out_items
@@ -6,5 +12,6 @@ class Number < ApplicationRecord
 
     ItemNotification
       .with(message: "#{Twemoji.render_unicode(RESPONSES.sample)}\n#{items_to_send}")
+      .deliver(self)
   end
 end
