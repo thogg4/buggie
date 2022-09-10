c5fe97ab85e69df7ad85ede58d73807c866ad230
Tim Hogg <thogg4@gmail.com>
2022-09-10 16:28:37 -0400

    db stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_09_10_191709) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "items", force: :cascade do |t|
     t.string "text"
     t.integer "number_id", null: false
