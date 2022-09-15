b47e3a81d4be27d4de044787f6f7ace52441bda8
Tim Hogg <thogg4@gmail.com>
2022-09-15 11:06:25 -0400

    only send on specific hours

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,4 +1,5 @@
 class Number < ApplicationRecord
+  HOURS_TO_SEND = [8, 10, 12, 2, 4, 6, 8, 10]
   has_many :items
 
   def send_out_items
@@ -6,8 +7,12 @@ class Number < ApplicationRecord
 
     message = items.not_complete.items_stringified_for_message
 
-    ItemNotification
-      .with(message: message)
-      .deliver(self)
+    Time.use_zone('Eastern Time (US & Canada)') do
+      return unless HOURS_TO_SEND.include?(Time.now.hour)
+
+      ItemNotification
+        .with(message: message)
+        .deliver(self)
+    end
   end
 end
